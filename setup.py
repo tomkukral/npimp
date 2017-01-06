2a0a74af47add2274491f64ad93d53d3feb14e5a
Tomáš Kukrál <tom@6shore.net>
2017-01-06 15:07:59 +0100

    fix repo url

--- setup.py
+++ setup.py
@@ -3,16 +3,15 @@ from setuptools import setup, find_packages
 with open('README') as f:
     long_description = ''.join(f.readlines())
 
-
 setup(
     name='npimp',
-    version='0.3',
+    version='0.4',
     description='Notebook regulation daemon',
     long_description=long_description,
     author='Tomáš Kukrál',
     author_email='tomas.kukral@6shore.net',
     license='MIT',
-    url='https://github.com/tomkukral/ntbpimp/',
+    url='https://github.com/tomkukral/npimp/',
     packages=find_packages(),
     zip_safe=False,
     install_requires=[],
